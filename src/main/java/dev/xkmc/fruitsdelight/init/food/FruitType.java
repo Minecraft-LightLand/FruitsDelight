02ec19620ec81702041757add157f71370013602
lcy0x1 <arthurlcy0x1@gmail.com>
2024-01-31 13:32:09 +0800

    color and model

--- src/main/java/dev/xkmc/fruitsdelight/init/food/FruitType.java
+++ src/main/java/dev/xkmc/fruitsdelight/init/food/FruitType.java
@@ -48,7 +48,7 @@ public enum FruitType {
 	SWEETBERRY(4, 0xC41A4F, () -> Items.SWEET_BERRIES, List.of()),
 	CHORUS(2, 0xD0A9D7, () -> Items.CHORUS_FRUIT, List.of(new EffectFunc(FDEffects.CHORUS::get, lv -> 1, lv -> 0, lv -> Math.min(1, lv * 0.05f)))),
 	BAYBERRY(4, 0x541135, FDTrees.BAYBERRY::getFruit, List.of(new EffectFunc(FDEffects.LEAF_PIERCING::get, lv -> lv * 80))),
-	KIWI(2, 0xFFFFFF/*TODO*/, FDTrees.KIWI::getFruit, List.of(new EffectFunc(FDEffects.CYCLING::get, lv -> lv * 4))),
+	KIWI(2, 0x89D90D, FDTrees.KIWI::getFruit, List.of(new EffectFunc(FDEffects.CYCLING::get, lv -> lv * 4))),
 	FIG(2, 0xE08517, FDTrees.FIG::getFruit, List.of(new EffectFunc(FDEffects.DIGESTING::get, lv -> lv * 80))),
 	;
 
