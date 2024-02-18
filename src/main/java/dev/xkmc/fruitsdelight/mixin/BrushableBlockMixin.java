15295e8c90f500191485aeb68ed7cd05306e63b5
lcy0x1 <arthurlcy0x1@gmail.com>
2024-02-17 22:52:06 -0500

    mixin fix

--- src/main/java/dev/xkmc/fruitsdelight/mixin/BrushableBlockMixin.java
+++ src/main/java/dev/xkmc/fruitsdelight/mixin/BrushableBlockMixin.java
@@ -4,6 +4,7 @@ import dev.xkmc.fruitsdelight.events.ClientEffectHandlers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
@@ -13,9 +14,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Pseudo
 @Mixin(targets = "com.min01.archaeology.block.BrushableBlock")
-public class BrushableBlockMixin {
+public abstract class BrushableBlockMixin extends BaseEntityBlock {
 
-	@Inject(at = @At("HEAD"), method = "animateTick", remap = true)
+	protected BrushableBlockMixin(Properties pProperties) {
+		super(pProperties);
+	}
+
+	@Inject(at = @At("HEAD"), method = "animateTick (Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/util/RandomSource;)V")
 	public void fruitsdelight$animateTick$durian(BlockState state, Level level, BlockPos pos, RandomSource random, CallbackInfo ci) {
 		if (!level.isClientSide()) return;
 		ClientEffectHandlers.suspiciousBlockAnimate(state, level, pos, random);
