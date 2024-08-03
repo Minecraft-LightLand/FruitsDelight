8f34ea622b4237ca779e37cb68f5c44b546b9522
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-08-04 00:03:09 +0800

    delete compat

--- src/main/java/dev/xkmc/fruitsdelight/compat/thirst/package-info.java
+++
@@ -1,8 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-
-package dev.xkmc.fruitsdelight.compat.thirst;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
