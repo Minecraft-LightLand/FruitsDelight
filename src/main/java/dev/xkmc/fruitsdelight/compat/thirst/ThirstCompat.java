82e7e7df40ec65fdb7d2c50a5906aa3bf0be8de9
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-08-11 22:42:43 +0800

    fix thirst compat

--- src/main/java/dev/xkmc/fruitsdelight/compat/thirst/ThirstCompat.java
+++ src/main/java/dev/xkmc/fruitsdelight/compat/thirst/ThirstCompat.java
@@ -1,33 +1,38 @@
 package dev.xkmc.fruitsdelight.compat.thirst;
 
-import dev.ghen.thirst.api.ThirstHelper;
+import dev.ghen.thirst.foundation.common.event.RegisterThirstValueEvent;
 import dev.xkmc.fruitsdelight.init.food.FDFood;
 import dev.xkmc.fruitsdelight.init.food.FDJuice;
 import dev.xkmc.fruitsdelight.init.food.FruitType;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ThirstCompat {
 
 	public static void init() {
+		MinecraftForge.EVENT_BUS.register(ThirstCompat.class);
+	}
+
+	@SubscribeEvent
+	public static void compat(RegisterThirstValueEvent event) {
 		for (var e : FDJuice.values()) {
-			ThirstHelper.addDrink(e.get(), 8, 13);
+			event.addDrink(e.get(), 8, 13);
 		}
 
-		ThirstHelper.addFood(FruitType.ORANGE.getFruit(), 4, 6);
-		ThirstHelper.addFood(FruitType.LYCHEE.getFruit(), 4, 6);
-		ThirstHelper.addFood(FruitType.PINEAPPLE.getFruit(), 4, 6);
-
-		ThirstHelper.addFood(FruitType.KIWI.getFruit(), 6, 10);
-		ThirstHelper.addFood(FruitType.PEACH.getFruit(), 6, 10);
-		ThirstHelper.addFood(FruitType.HAMIMELON.getFruit(), 6, 10);
-		ThirstHelper.addFood(FruitType.MANGO.getFruit(), 6, 10);
-
-		ThirstHelper.addFood(FruitType.PEAR.getFruit(), 10, 16);
-		ThirstHelper.addFood(FDFood.BAKED_PEAR.get(), 10, 16);
-		ThirstHelper.addFood(FDFood.HAMIMELON_POPSICLE.get(), 10, 16);
-		ThirstHelper.addFood(FDFood.HAMIMELON_SHAVED_ICE.get(), 10, 16);
-		ThirstHelper.addFood(FDFood.KIWI_POPSICLE.get(), 10, 16);
-		ThirstHelper.addFood(FDFood.FIG_CHICKEN_STEW.get(), 10, 16);
-		ThirstHelper.addFood(FDFood.PEAR_WITH_ROCK_SUGAR.get(), 14, 20);
+		event.addFood(FruitType.ORANGE.getFruit(), 4, 6);
+		event.addFood(FruitType.LYCHEE.getFruit(), 4, 6);
+		event.addFood(FruitType.PINEAPPLE.getFruit(), 4, 6);
+		event.addFood(FruitType.KIWI.getFruit(), 4, 6);
+		event.addFood(FruitType.PEACH.getFruit(), 4, 6);
+		event.addFood(FruitType.HAMIMELON.getFruit(), 4, 6);
+		event.addFood(FruitType.MANGO.getFruit(), 4, 6);
+		event.addFood(FruitType.PEAR.getFruit(), 6, 10);
+		event.addFood(FDFood.BAKED_PEAR.get(), 8, 13);
+		event.addFood(FDFood.HAMIMELON_POPSICLE.get(), 8, 13);
+		event.addFood(FDFood.KIWI_POPSICLE.get(), 8, 13);
+		event.addFood(FDFood.HAMIMELON_SHAVED_ICE.get(), 10, 16);
+		event.addFood(FDFood.FIG_CHICKEN_STEW.get(), 10, 16);
+		event.addFood(FDFood.PEAR_WITH_ROCK_SUGAR.get(), 14, 20);
 	}
 
 }
